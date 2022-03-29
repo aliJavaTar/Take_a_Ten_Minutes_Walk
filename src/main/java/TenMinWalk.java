49a726f576e15a9331b32323d4c285352f7dc0c2
alijavaboy <ali77erfagh99@gmail.com>
2022-03-29 12:11:45 +0430

    refactor

--- src/main/java/TenMinWalk.java
+++ src/main/java/TenMinWalk.java
@@ -1,30 +1,28 @@
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TenMinWalk {
-
     public boolean isValid(char[] walk) {
-        if (validCharacter(walk, walk.length))
-        {
-            int alongNorth_South = 0;
-            int widthWestEast = 0;
-            for (char walkChar : walk)
-            {
-                if (walkChar == 'n')
-                    alongNorth_South++;
-                else if (walkChar == 's')
-                    alongNorth_South--;
-                else if (walkChar == 'w')
-                    widthWestEast++;
-                else if (walkChar == 'e')
-                    widthWestEast--;
-            }
-            return alongNorth_South == 0 && widthWestEast == 0;
+        if (isValidCharacter(walk, walk.length)) {
+            return isThenMin(walk);
         } else return false;
 
     }
 
-    public boolean validCharacter(char[] walk, int length) {
+    public boolean isThenMin(char[] walks) {
+        int alongNorth_South = 0;
+        int widthWestEast = 0;
+        for (char walkChar : walks) {
+            switch (walkChar) {
+                case 'n' -> alongNorth_South++;
+                case 's' -> alongNorth_South--;
+                case 'w' -> widthWestEast++;
+                case 'e' -> widthWestEast--;
+            }
+        }
+        return alongNorth_South == 0 && widthWestEast == 0;
+    }
+
+    public boolean isValidCharacter(char[] walk, int length) {
         String text = new String(walk);
         System.out.println(text);
         String regex = "[wnse]+";
